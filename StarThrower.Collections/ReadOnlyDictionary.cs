69123289af5268c9c3c405e2c32b774f3dc2201d
Stephen Elmer <steve.elmer@starthrower.net>
2026-05-31 11:48:41 -0500

    Phase 1, Step 2d (Collections): BCL Supersedence

--- StarThrower.Collections/ReadOnlyDictionary.cs
+++ StarThrower.Collections/ReadOnlyDictionary.cs
@@ -1,4 +1,4 @@
-﻿/***********************************************************************************
+/***********************************************************************************
     StarThrower Utilities / Collections
     Copyright (C) 2005-2026  Stephen Elmer
 
@@ -19,133 +19,15 @@
 
 using System;
 using System.Collections.Generic;
-using System.Diagnostics.CodeAnalysis;
 
 namespace StarThrower.Collections
 {
-    public class ReadOnlyDictionary<TKey, TValue> : IDictionary<TKey, TValue>
+    [Obsolete("Use System.Collections.ObjectModel.ReadOnlyDictionary<TKey, TValue> instead.")]
+    public class ReadOnlyDictionary<TKey, TValue> : System.Collections.ObjectModel.ReadOnlyDictionary<TKey, TValue>
         where TKey : notnull
     {
-        #region [ Private Instance Variables ]
+        public ReadOnlyDictionary() : base(new Dictionary<TKey, TValue>()) { }
 
-        private IDictionary<TKey, TValue> _dictionary;
-
-        #endregion
-
-
-        #region [ Construction ]
-
-        public ReadOnlyDictionary()
-        {
-            _dictionary = new Dictionary<TKey, TValue>();
-        }
-
-        public ReadOnlyDictionary(IDictionary<TKey, TValue> dictionary)
-        {
-            _dictionary = dictionary;
-        }
-
-        #endregion
-
-
-        #region [ IDictionary<TKey,TValue> Members ]
-
-        public void Add(TKey key, TValue value)
-        {
-            throw new NotSupportedException("This dictionary is read-only");
-        }
-
-        public bool ContainsKey(TKey key)
-        {
-            return _dictionary.ContainsKey(key);
-        }
-
-        public ICollection<TKey> Keys
-        {
-            get { return _dictionary.Keys; }
-        }
-
-        public bool Remove(TKey key)
-        {
-            throw new NotSupportedException("This dictionary is read-only");
-        }
-
-        public bool TryGetValue(TKey key, [MaybeNullWhen(false)] out TValue value)
-        {
-            return _dictionary.TryGetValue(key, out value);
-        }
-
-        public ICollection<TValue> Values
-        {
-            get { return _dictionary.Values; }
-        }
-
-        public TValue this[TKey key]
-        {
-            get { return _dictionary[key]; }
-            set { throw new NotSupportedException("This dictionary is read-only"); }
-        }
-
-        #endregion
-
-
-        #region [ ICollection<KeyValuePair<TKey,TValue>> Members ]
-
-        public void Add(KeyValuePair<TKey, TValue> item)
-        {
-            throw new NotSupportedException("This dictionary is read-only");
-        }
-
-        public void Clear()
-        {
-            throw new NotSupportedException("This dictionary is read-only");
-        }
-
-        public bool Contains(KeyValuePair<TKey, TValue> item)
-        {
-            return _dictionary.Contains(item);
-        }
-
-        public void CopyTo(KeyValuePair<TKey, TValue>[] array, int arrayIndex)
-        {
-            _dictionary.CopyTo(array, arrayIndex);
-        }
-
-        public int Count
-        {
-            get { return _dictionary.Count; }
-        }
-
-        public bool IsReadOnly
-        {
-            get { return true; }
-        }
-
-        public bool Remove(KeyValuePair<TKey, TValue> item)
-        {
-            throw new NotSupportedException("This dictionary is read-only");
-        }
-
-        #endregion
-
-
-        #region [ IEnumerable<KeyValuePair<TKey,TValue>> Members ]
-
-        public IEnumerator<KeyValuePair<TKey, TValue>> GetEnumerator()
-        {
-            return _dictionary.GetEnumerator();
-        }
-
-        #endregion
-
-
-        #region [ IEnumerable Members ]
-
-        System.Collections.IEnumerator System.Collections.IEnumerable.GetEnumerator()
-        {
-            return (_dictionary as System.Collections.IEnumerable).GetEnumerator();
-        }
-
-        #endregion
+        public ReadOnlyDictionary(IDictionary<TKey, TValue> dictionary) : base(dictionary) { }
     }
 }
