3e0da7be064c05a28ae7d96f1babfdf32b1ae6d9
Stephen Elmer <steve.elmer@starthrower.net>
2026-06-09 19:18:46 -0500

    Phase 1, Steps 5 (various): updated header on all source files

--- StarThrower.Gis.GeoUtilities/Formatting/IDmsFormatter.cs
+++ StarThrower.Gis.GeoUtilities/Formatting/IDmsFormatter.cs
@@ -1,21 +1,4 @@
-/***********************************************************************************
-    StarThrower Utilities / Gis.GeoUtilities
-    Copyright (C) 2005-2026  Stephen Elmer
-
-    This library is free software; you can redistribute it and/or
-    modify it under the terms of the GNU Lesser General Public
-    License as published by the Free Software Foundation; either
-    version 2.1 of the License, or (at your option) any later version.
-
-    This library is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-    Lesser General Public License for more details.
-
-    You should have received a copy of the GNU Lesser General Public
-    License along with this library; if not, write to the Free Software
-    Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
-***********************************************************************************/
+ï»¿// Copyright Â© 2005-2026 Stephen Elmer. Licensed under the MIT License.
 
 using System;
 
@@ -24,7 +7,7 @@ namespace StarThrower.Gis.GeoUtilities.Formatting
     public enum DmsFormat
     {
         Default = 0, //see DMS1
-        Dms1 = 1, //[±][d][d]d° [m]m' ss.ss"  examples:  31° 56' 31.13"    -31° 56' 31.13"
+        Dms1 = 1, //[ï¿½][d][d]dï¿½ [m]m' ss.ss"  examples:  31ï¿½ 56' 31.13"    -31ï¿½ 56' 31.13"
         Dms2 = 2, //{N|n|E|e|S|s|w|w}[d][d]d{D|d}[m]m{M|m}[s]s[.s[s]]{S|s}  examples:  S31d56m31.13s  n31D56M3.1S   E3d2m3s
     }
 
